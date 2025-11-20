2c8c17f0d58ad333dce9617de605cd3e4a9caa5a
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-20 18:08:58 +0700

    menambahkan composable TampilSiswa dengan statusUiSiswa dan aksi back

---
+++ app/src/main/java/com/example/prak6/view/TampilData.kt
@@ -0,0 +1,11 @@
+package com.example.prak6.view
+
+import androidx.compose.runtime.Composable
+import com.example.prak6.model.Siswa
+
+
+@Composable
+fun TampilSiswa(
+    statusUiSiswa: Siswa,
+    onBackButtonClicked:()->Unit
+){}
