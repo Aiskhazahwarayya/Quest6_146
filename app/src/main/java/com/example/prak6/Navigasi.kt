46b138df6567271fe52731bc03ac835bd8fd8674
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-20 18:33:40 +0700

    menambahkan NavHost dalam Scaffold dan navigasi dari Formulir ke Detail

--- app/src/main/java/com/example/prak6/Navigasi.kt
+++ app/src/main/java/com/example/prak6/Navigasi.kt
@@ -1,11 +1,19 @@
 package com.example.prak6
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.prak6.model.DataJK.JenisK
 import com.example.prak6.viewmodel.SiswaViewModel
+import com.example.praktikum6.view.FormSiswa
 
 enum class Navigasi {
     Formulir,
@@ -17,4 +25,27 @@ fun SiswaApp(
     navController: NavHostController = rememberNavController(),
     viewModel: SiswaViewModel = viewModel(),
     modifier: Modifier
-){}
+) {
+    Scaffold { isiRuang ->
+        val uiState = viewModel.statusUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+
+            modifier = Modifier.padding(isiRuang)
+        ) {
+            composable(route = Navigasi.Formulir.name) {
+                val konteks = LocalContext.current
+                FormSiswa(
+                    pilihanJK = JenisK.map { id -> konteks.resources.getString(id) },
+                    onSubmitButtonClicked = {
+                        viewModel.setSiswa(it)
+                        navController.navigate(Navigasi.Detail.name)
+
+                    }
+                )
+            }
+
+        }
+    }
+}
