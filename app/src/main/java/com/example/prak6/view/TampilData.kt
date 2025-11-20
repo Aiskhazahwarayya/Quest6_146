faeea4502fe2cba5ea3649bd9a2243eebc1f6a22
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-20 18:10:09 +0700

    menambahkan list items untuk menampilkan nama, gender, dan alamat siswa

--- app/src/main/java/com/example/prak6/view/TampilData.kt
+++ app/src/main/java/com/example/prak6/view/TampilData.kt
@@ -1,11 +1,21 @@
 package com.example.prak6.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import com.example.prak6.model.Siswa
+import com.example.prak6.R
+
 
 
 @Composable
 fun TampilSiswa(
     statusUiSiswa: Siswa,
     onBackButtonClicked:()->Unit
-){}
+){
+    val items = listOf(
+        Pair(stringResource(R.string.nama), statusUiSiswa.nama),
+        Pair(stringResource(id = R.string.gender), statusUiSiswa.gender),
+        Pair(stringResource(R.string. alamat), statusUiSiswa.alamat)
+    )
+
+}
