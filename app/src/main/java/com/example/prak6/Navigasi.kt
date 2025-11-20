d0ae1230a59cda1cd2026960d781a3fc13595e01
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-20 18:18:13 +0700

    menambahkan composable SiswaApp dengan NavController dan ViewModel

--- app/src/main/java/com/example/prak6/Navigasi.kt
+++ app/src/main/java/com/example/prak6/Navigasi.kt
@@ -1,6 +1,20 @@
 package com.example.prak6
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.prak6.viewmodel.SiswaViewModel
+
 enum class Navigasi {
     Formulir,
     Detail
 }
+
+@Composable
+fun SiswaApp(
+    navController: NavHostController = rememberNavController(),
+    viewModel: SiswaViewModel = viewModel(),
+    modifier: Modifier
+){}
