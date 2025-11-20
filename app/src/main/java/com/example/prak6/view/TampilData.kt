1aa0bce5509a70d7ccfb7e3aa937de3a0e7b9b97
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-20 18:13:45 +0700

    menampilkan detail siswa dalam Column dengan list items dan tombol Back

--- app/src/main/java/com/example/prak6/view/TampilData.kt
+++ app/src/main/java/com/example/prak6/view/TampilData.kt
@@ -1,5 +1,13 @@
 package com.example.prak6.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -7,7 +15,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.prak6.model.Siswa
 import com.example.prak6.R
 
@@ -32,6 +45,34 @@ fun TampilSiswa(
             )
         }
 
-    ){}
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+            ) {
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_small)))
+                        Text(text = item.second, fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                    }
+                    Divider(thickness = dimensionResource(id = R.dimen.thickness_divider))
+                }
+                Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_small)))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackButtonClicked
+                ) {
+                    Text(stringResource(id = R.string.back))
+                }
+
+            }
+
+        }
+    }
 
 }
